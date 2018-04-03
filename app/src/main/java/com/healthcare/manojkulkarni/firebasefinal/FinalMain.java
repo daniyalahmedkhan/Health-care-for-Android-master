1f362fb8fd3beb183b961f06051f52a7f50d1e74
daniyalahmedkhan <daniyalahmed520@gmail.com>
2018-04-04 00:58:05 +0500

    Initial commit

---
+++ app/src/main/java/com/healthcare/manojkulkarni/firebasefinal/FinalMain.java
@@ -0,0 +1,15 @@
+package com.healthcare.manojkulkarni.firebasefinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.healthcare.R;
+
+public class FinalMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_main);
+    }
+}
